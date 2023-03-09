98e1a6ac77d917275b8b56988f35d0a06226d075
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-03-09 02:22:12 +0200

    get api for services data

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Services;
 
 class ServiceController extends Controller
 {
     //
+    public function ServiceView(){
+        $result = Services::all();
+        return $result;
+    }
 }
